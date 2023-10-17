f671a719bd8d93dc209786caa3b07880391bb120
dhruvum <117373442+dhruvum@users.noreply.github.com>
2023-10-17 13:41:18 -0400

    Update region_occurrence3.py

--- region_occurrence3.py
+++ region_occurrence3.py
@@ -1,17 +1,13 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-import seaborn as sns
-import scipy.io #This will allow us to open .mat files
 
 #0.4 - 50
 #0.6 - 26
 #1.2 - 10
 
-#flytable = pd.read_pickle(r"C:\Users\Dhruvum Bajpai\Desktop\Computational Project\My Scripts\Python Conversion\Maps\2\flytable.pkl")
 flytable = pd.read_pickle(r"C:\Users\Dhruvum Bajpai\Desktop\Computational Project\Ensemble\flytable_ensemble.pkl")
 
-#regions = 25
 reactLength = 156
 
 #%% Plots length of reactions
@@ -26,33 +22,28 @@ flytable_filter = flytable_filter[(flytable_filter.stimProtocol=='loom_10to180_l
 #flytable_filter = flytable_filter[(flytable_filter.stimProtocol=='loom_10to180_lv40_blackonwhite.mat\n')&(flytable_filter.labels_simp==1)].reset_index(drop=True)
 
 #flytable_filter = flytable_filter.iloc[0:20]
+
 #%% Make Matrix of Region occurrence among all Trials
 matrix = np.zeros((len(flytable_filter.trial_list), reactLength))
 matrix[:] = np.nan
-
-maxRegion = 0
 
 for i in range(len(flytable_filter.trial_list)):
     full_trial = flytable_filter.trial_full[i]
     stimStart = flytable_filter.frameofStimStart[i]
     midEndFrame = flytable_filter.midEndFrame[i]
-    
-    maxTrial = max(full_trial)
-    if maxTrial > maxRegion:
-        maxRegion = maxTrial
         
     if (len(full_trial)+stimStart>=midEndFrame) & (midEndFrame>stimStart):
         #sum((len(flytable_filter.trial_full)>=flytable_filter.midEndFrame))
         #sum(flytable_filter.midEndFrame>flytable_filter.frameofStimStart)
         diff = reactLength - flytable_filter.trial_length[i]
         if diff>0:
-            matrix[i][diff:-1] = full_trial
+            matrix[i][diff-1:-1] = full_trial[0:midEndFrame-stimStart]
         else:
-            matrix[i] = full_trial[0 - diff:reactLength + diff]
+            matrix[i] = full_trial[-diff:midEndFrame-stimStart]
             #Remember, trial_full begins assigning regions at the frame of stim start.
-                
-        
+         
 #%% Matrix of Region Count
+maxRegion = np.nanmax(matrix).astype(int)
 regions = maxRegion + 1
 
 reactMatrix = np.zeros((regions-1, reactLength))
